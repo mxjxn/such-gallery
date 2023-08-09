3435665a4b736da4143a152ed77fb72b1f235955
MXJXN <modulations@gmail.com>
2023-08-09 15:41:51 -0400

    removing old deprecated file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import { useProfile } from "@/hooks/useProfile";
 import { useAccount } from "wagmi";
 import { useEffect } from "react";
-// import SubmitArt from "@/components/SubmitArt";
 import NFTSearch from "@/components/NFTSearch";
 import UserNftList from "@/components/UserNftList";
 import UserCuratedLists from "@/components/UserCuratedLists";
@@ -11,13 +10,11 @@ import UserCuratedLists from "@/components/UserCuratedLists";
 export default function Home() {
   const { user } = useProfile();
   const { isDisconnected } = useAccount();
-	console.log('poop')
   return (
     <main className="">
       {!isDisconnected && user?.ethAddress && (
         <div className="w-full flex justify-around">
           <div className="w-1/2">
-            {/* <SubmitArt /> */}
 						<NFTSearch />
             <UserNftList address={user.ethAddress} />
 						<UserCuratedLists />
